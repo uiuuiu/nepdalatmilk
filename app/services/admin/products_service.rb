ea1e28ab9bd89b55e1adc79979be241fa0e3ad5b
at-dypd <dy.pham@asiantech.vn>
2017-06-03 17:17:37 +0700

    [Add] product edit

--- app/services/admin/products_service.rb
+++
@@ -1,9 +0,0 @@
-class Admin::ProductsService < BaseService
-  def condition
-    @q = Product.ransack(params[:q])
-  end
-
-  def item_list page, limit
-    @q.result.page(page).per(limit)
-  end
-end
